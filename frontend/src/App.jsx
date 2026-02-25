a52e9d16cad89b1e3125f45aaeb8464fcd4db477
dimpalagarwal <alwardimpal12@gmail.com>
2026-02-25 22:28:10 +0530

    fixed app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,7 @@ import Layout from "./components/Layout"
 import Records from "./pages/Records"
 import DocumentView from "./pages/DocumentView"
 import SecurityConsole from "./pages/SecurityConsole"
-import IntegrityMonitor from "./pages/IntegrityMonitor"
 import ActivityLogs from "./pages/ActivityLogs"
-import Settings from "./pages/Settings"
 
 function App() {
   return (
@@ -14,9 +12,7 @@ function App() {
         <Route path="/" element={<Records />} />
         <Route path="/document/:id" element={<DocumentView />} />
         <Route path="/security" element={<SecurityConsole />} />
-        <Route path="/integrity" element={<IntegrityMonitor />} />
         <Route path="/logs" element={<ActivityLogs />} />
-        <Route path="/settings" element={<Settings />} />
       </Routes>
     </Layout>
   )
